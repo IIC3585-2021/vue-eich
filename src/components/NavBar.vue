93adfe854c29681d794cf12c1180295e3c3d3f0f
vchadwick <vchadwick@uc.cl>
2021-06-06 18:00:36 -0400

    css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="nav-bar">
-        <router-link :to="{ name: 'Hello' }">Home</router-link>
-        <router-link :to="{ name: 'Document' }">Documentos</router-link>
-        <router-link :to="{ name: 'Search' }">Buscador</router-link>
-        <router-link :to="{ name: 'Favourites' }">Favourites</router-link>
+        <router-link :to="{ name: 'Hello' }" class="logo">WikiEich</router-link>
+        <router-link :to="{ name: 'Document' }" class="other">Documentos</router-link>
+        <router-link :to="{ name: 'Search' }" class="other">Buscador</router-link>
+        <router-link :to="{ name: 'Favourites' }" class="other">Favourites</router-link>
         <!-- <router-link :to="{ name: 'Questions' }">Preguntas - Respuestas</router-link> -->
 
     </div> 
